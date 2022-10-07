295429faa7230d68281ae18be053192a522d8794
Alejandro Suarez <asuarez@iri.upc.edu>
2022-10-07 18:07:30 +0200

    randu planes

--- src/pi_estimation.cpp
+++ src/pi_estimation.cpp
@@ -24,13 +24,15 @@ using namespace std;
 
 constexpr int k_number_of_points = 1000000;
 
-using Random = ash_rand::pcg32;
+//using Random = ash_rand::ran;
+using Random = std::minstd_rand0;
 
 int main() {
-    //uint64_t seed = ash_rand::produce_random_seed();
-    uint64_t seed = 42;
+    uint64_t seed = ash_rand::produce_random_seed();
+    //uint64_t seed = 42;
     Random rng(seed);
 
+    // VERSION WITH CIRCLE
     //int count_inside_circle = 0;
     //std::uniform_real_distribution unif_dist(-1.0, 1.0);
     //for (int i = 0; i < k_number_of_points; ++i) {
@@ -40,23 +42,25 @@ int main() {
         //double dist_sq = x*x + y*y;
         //count_inside_circle += dist_sq < 1;
     //}
+    //float pi_hat = (4.0/k_number_of_points) * count_inside_circle;
     
-    
+    // VERSION WITH SPHERE
     int count_inside_sphere = 0;
-    std::uniform_real_distribution unif_dist(-1.0, 1.0);
+    std::uniform_real_distribution<float> unif_dist(-1.0, 1.0);
     for (int i = 0; i < k_number_of_points; ++i) {
-        double x = unif_dist(rng);
-        double y = unif_dist(rng);
-        double z = unif_dist(rng);
+        float x = unif_dist(rng);
+        float y = unif_dist(rng);
+        float z = unif_dist(rng);
 
-        double dist_sq = x*x + y*y + z*z;
+        //cerr << x << ' ' << y << ' ' << z << '\n';
+
+        float dist_sq = x*x + y*y + z*z;
         count_inside_sphere += dist_sq < 1;
     }
+    float pi_hat = (6.0/k_number_of_points) * count_inside_sphere;
 
-    //double pi_hat = (4.0/k_number_of_points) * count_inside_circle;
-    double pi_hat = (6.0/k_number_of_points) * count_inside_sphere;
-    double error_abs = std::fabs(pi_hat - M_PI);
-    double error_rel = 100 * error_abs / M_PI;
+    float error_abs = std::fabs(pi_hat - M_PI);
+    float error_rel = 100 * error_abs / M_PI;
     cout << "Pi estimation: " << pi_hat << endl;
     cout << "Absolute error: " << error_abs << endl;
     cout << "Relative error: " << error_rel << "%" << endl;
